a0d7d143455ace5b2703cb62369f8ec8920235f1
Bryan Berge <berge.bryan@gmail.com>
2024-03-25 15:53:35 +0100

    UI

---
+++ Assets/Scripts/CanvaManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class CanvaManager : MonoBehaviour
+{
+    public void EtatCanva(GameObject _gameObject)
+    {
+        if (_gameObject.activeSelf)
+        {
+            _gameObject.SetActive(false);
+        }
+        else
+        {
+            _gameObject.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/SceneChanged.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanged : MonoBehaviour
+{
+    public void SceneSwitch(string _NameOfScene)
+    {
+        SceneManager.LoadScene(_NameOfScene);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
